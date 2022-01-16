670f3d7e145e2152f5273672caf072695b54bc20
lusalamark <lusalamark63@gmail.com>
2022-01-16 21:30:28 +0300

    Designing the checkout page and the subtotal fuctionality

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from "react";
+import { Route, Routes } from "react-router";
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
+import Checkout from "./Checkout";
 import Header from "./Header.js";
 import Home from "./Home";
 
 function App() {
   return (
-    //BEM
-    <div className="app">
+    <BrowserRouter>
       <Header />
-      {/*Header*/}
-      <Home />
-      {/*Home*/}
-    </div>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/checkout" element={<Checkout />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Checkout.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Checkout.css";
+import Subtotal from "./Subtotal";
+
+function Checkout() {
+  return (
+    <div className="checkout">
+      <div className="checkout__left">
+        <img
+          className="checkout__ad"
+          src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+        />
+
+        <div>
+          <h2>Basket is empty</h2>
+          <p>You have no items in your basket.</p>
+        </div>
+
+        <div>
+          <h2 className="checkout__title">Your shopping basket</h2>
+        </div>
+      </div>
+
+      <div className="checkout__right">
+        <Subtotal />
+      </div>
+    </div>
+  );
+}
+
+export default Checkout;
