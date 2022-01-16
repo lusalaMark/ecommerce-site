2c4696aad499a08c6f0ad78b060f8ab6ac797ddc
lusalamark <lusalamark63@gmail.com>
2022-01-16 13:31:13 +0300

    setting up firebase hosting platform for the web app.

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDM2it-cdR-JB4J8brytEgDVF3v59OF7A4",
+    authDomain: "ecommerce-site-a8d87.firebaseapp.com",
+    projectId: "ecommerce-site-a8d87",
+    storageBucket: "ecommerce-site-a8d87.appspot.com",
+    messagingSenderId: "702977184549",
+    appId: "1:702977184549:web:4721dfceff6990ea7800cc",
+    measurementId: "G-6TSV8X6HKX"
+  };
